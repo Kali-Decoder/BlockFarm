b7efb0910e9a1b483557f193a778500ba21144c8
Kali-Decoder <neerajchoubisa876@gmail.com>
2022-09-07 22:23:58 +0530

    Making contract

---
+++ test/supply.test.js
@@ -0,0 +1,6 @@
+const CoffeeSupplyChain = artifacts.require("CoffeeSupplyChain.sol");
+
+const SupplyChainStorage = artifacts.require("SupplyChainStorage.sol");
+
+const SupplyChainUser = artifacts.require("SupplyChainUser.sol");
+
